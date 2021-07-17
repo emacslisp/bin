a0e0434dfed9b2baaecb98554da476f29aac96f7
emacslisp <java.gdb@gmail.com>
2021-07-17 20:05:02 +1000

    rename and update bat file

--- win/devenv15.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call cmd /c start "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" %*
--- win/devenv19.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call cmd /c start "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe" %*
